0290a79a6abe3b101a4865654dbe2e3756b6953c
Cornelius <cornelius.udeh@yahoo.com>
2024-11-23 13:43:36 +0100

    lint: code clean up

--- public/class-wis-events-public.php
+++ public/class-wis-events-public.php
@@ -1,39 +1,43 @@
 <?php
 
-class WIS_Events_Public {
+class WIS_Events_Public
+{
     private $plugin_name;
     private $version;
 
-    public function __construct($plugin_name, $version) {
+    public function __construct($plugin_name, $version)
+    {
         $this->plugin_name = $plugin_name;
         $this->version = $version;
     }
 
-    public function enqueue_styles() {
+    public function enqueue_styles()
+    {
         wp_enqueue_style($this->plugin_name, plugin_dir_url(__FILE__) . 'assets/css/wis-events-public.css', array(), $this->version, 'all');
     }
 
-    public function render_events_shortcode($atts) {
+    public function render_events_shortcode($atts)
+    {
         // Parse any attributes passed to the shortcode
         $atts = shortcode_atts([
             'number' => 5, // Default: show 5 events
         ], $atts, 'wis_events');
-    
+
         // Query the events
         $query_args = [
-            'post_type'      => 'event',
+            'post_type' => 'wis_event',
             'posts_per_page' => $atts['number'],
-            'meta_key'       => '_wis_event_start_date',
-            'orderby'        => 'meta_value',
-            'order'          => 'ASC',
+            'meta_key' => '_wis_event_start_date',
+            'orderby' => 'meta_value',
+            'order' => 'ASC',
         ];
-    
+
         $events = new WP_Query($query_args);
-    
+
         if (!$events->have_posts()) {
             return '<p>No events found.</p>';
         }
-    
+
         // Build the HTML output
         $output = '<div class="wis-events-list">';
         while ($events->have_posts()) {
@@ -43,9 +47,9 @@ class WIS_Events_Public {
             $start_time = get_post_meta(get_the_ID(), '_wis_event_start_time', true);
             $end_time = get_post_meta(get_the_ID(), '_wis_event_end_time', true);
             $location = get_post_meta(get_the_ID(), '_wis_event_location', true);
-    
+
             $output .= '<div class="wis-event">';
-            $output .= '<h3>' . esc_html(get_the_title()) . '</h3>';
+            $output .= '<h3> <a href="'. get_the_permalink(get_the_ID()) .'">' . esc_html(get_the_title()) . '</a></h3>';
             $output .= '<p><strong>Start Date:</strong> ' . esc_html($start_date) . '</p>';
             $output .= '<p><strong>End Date:</strong> ' . esc_html($end_date) . '</p>';
             $output .= '<p><strong>Time:</strong> ' . esc_html($start_time) . ' - ' . esc_html($end_time) . '</p>';
@@ -55,7 +59,7 @@ class WIS_Events_Public {
         }
         wp_reset_postdata();
         $output .= '</div>';
-    
+
         return $output;
     }
 }
